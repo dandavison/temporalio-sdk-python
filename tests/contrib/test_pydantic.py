e7b39b164b56bc79708839ae3a2c9701ecec7870
Dan Davison <dan.davison@temporal.io>
2025-01-30 16:25:23 -0500

    Delete special datetime hadling

--- tests/contrib/test_pydantic.py
+++ tests/contrib/test_pydantic.py
@@ -1,4 +1,3 @@
-import dataclasses
 import uuid
 from datetime import datetime, timedelta
 from ipaddress import IPv4Address
@@ -10,10 +9,6 @@ from temporalio import activity, workflow
 from temporalio.client import Client
 from temporalio.contrib.pydantic.converter import pydantic_data_converter
 from temporalio.worker import Worker
-from temporalio.worker.workflow_sandbox import (
-    SandboxedWorkflowRunner,
-    SandboxRestrictions,
-)
 
 
 class MyPydanticModel(BaseModel):
@@ -37,29 +32,6 @@ class MyWorkflow:
         )
 
 
-# Due to known issues with Pydantic's use of issubclass and our inability to
-# override the check in sandbox, Pydantic will think datetime is actually date
-# in the sandbox. At the expense of protecting against datetime.now() use in
-# workflows, we're going to remove datetime module restrictions. See sdk-python
-# README's discussion of known sandbox issues for more details.
-def new_sandbox_runner() -> SandboxedWorkflowRunner:
-    # TODO(cretz): Use with_child_unrestricted when https://github.com/temporalio/sdk-python/issues/254
-    # is fixed and released
-    invalid_module_member_children = dict(
-        SandboxRestrictions.invalid_module_members_default.children
-    )
-    del invalid_module_member_children["datetime"]
-    return SandboxedWorkflowRunner(
-        restrictions=dataclasses.replace(
-            SandboxRestrictions.default,
-            invalid_module_members=dataclasses.replace(
-                SandboxRestrictions.invalid_module_members_default,
-                children=invalid_module_member_children,
-            ),
-        )
-    )
-
-
 async def test_workflow_with_pydantic_model(client: Client):
     # Replace data converter in client
     new_config = client.config()
@@ -81,7 +53,6 @@ async def test_workflow_with_pydantic_model(client: Client):
         task_queue=task_queue_name,
         workflows=[MyWorkflow],
         activities=[my_activity],
-        workflow_runner=new_sandbox_runner(),
     ):
         result = await client.execute_workflow(
             MyWorkflow.run,
