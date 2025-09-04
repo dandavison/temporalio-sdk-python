0a0db3bf8994ca044a4ed6b18251b324e7be2e77
Dan Davison <dan.davison@temporal.io>
2025-09-04 17:31:19 -0400

    WIP

--- tests/nexus/test_worker.py
+++ tests/nexus/test_worker.py
@@ -7,54 +7,66 @@ import uuid
 from datetime import timedelta
 
 import nexusrpc.handler
+import pytest
 
+import temporalio.exceptions
 from temporalio import workflow
 from temporalio.client import Client
 from tests.helpers import new_worker
 from tests.helpers.nexus import create_nexus_endpoint, make_nexus_endpoint_name
 
 
-@nexusrpc.handler.service_handler
-class EchoService:
-    @nexusrpc.handler.sync_operation
-    async def echo(
-        self, _ctx: nexusrpc.handler.StartOperationContext, input: str
-    ) -> str:
-        return input
-
-
-@workflow.defn
+@workflow.defn(sandboxed=False)
 class NexusCallerWorkflow:
     """Workflow that calls a Nexus operation."""
 
     @workflow.run
-    async def run(self, input: str) -> str:
+    async def run(self, id: int) -> None:
         nexus_client = workflow.create_nexus_client(
             endpoint=make_nexus_endpoint_name(workflow.info().task_queue),
-            service=EchoService,
+            service="MaxConcurrentTestService",
         )
 
-        return await nexus_client.execute_operation(
-            EchoService.echo,
-            input,
-            schedule_to_close_timeout=timedelta(seconds=10),
-        )
+        try:
+            return await nexus_client.execute_operation(
+                "op",
+                id,
+                schedule_to_close_timeout=timedelta(milliseconds=500),
+            )
+        except Exception as err:
+            assert isinstance(err.__cause__, temporalio.exceptions.TimeoutError)
 
 
-async def test_max_concurrent_nexus_tasks(client: Client):
+@pytest.mark.parametrize("max_concurrent_nexus_tasks", [1])
+async def test_max_concurrent_nexus_tasks(
+    client: Client, max_concurrent_nexus_tasks: int
+):
+    ids = []
+    event = asyncio.Event()
+
+    @nexusrpc.handler.service_handler
+    class MaxConcurrentTestService:
+        @nexusrpc.handler.sync_operation
+        async def op(
+            self, _ctx: nexusrpc.handler.StartOperationContext, id: int
+        ) -> None:
+            ids.append(id)
+            await event.wait()
+
     async with new_worker(
         client,
         NexusCallerWorkflow,
-        nexus_service_handlers=[EchoService()],
+        nexus_service_handlers=[MaxConcurrentTestService()],
+        max_concurrent_nexus_tasks=max_concurrent_nexus_tasks,
     ) as worker:
         await create_nexus_endpoint(worker.task_queue, client)
 
-        await asyncio.wait_for(
-            client.execute_workflow(
+        for i in range(10):
+            await client.execute_workflow(
                 NexusCallerWorkflow.run,
-                "input",
+                i,
                 id=str(uuid.uuid4()),
                 task_queue=worker.task_queue,
-            ),
-            timeout=2.0,
-        )
+            )
+        event.set()
+        print(ids)
