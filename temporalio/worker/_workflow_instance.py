87756c84899b50be1239a096e912cc88eefe7753
Dan Davison <dan.davison@temporal.io>
2025-09-10 23:08:25 -0400

    fix: Wire context for signals, updates, and queries

--- temporalio/worker/_workflow_instance.py
+++ temporalio/worker/_workflow_instance.py
@@ -576,12 +576,17 @@ class _WorkflowInstanceImpl(  # type: ignore[reportImplicitAbstractClass]
                 self._in_progress_updates[job.id] = HandlerExecution(
                     job.name, defn.unfinished_policy, job.id
                 )
+                context = temporalio.converter.WorkflowSerializationContext(
+                    namespace=self._info.namespace,
+                    workflow_id=self._info.workflow_id,
+                )
                 args = self._process_handler_args(
                     job.name,
                     job.input,
                     defn.name,
                     defn.arg_types,
                     defn.dynamic_vararg,
+                    context,
                 )
                 handler_input = HandleUpdateInput(
                     id=job.id,
@@ -600,6 +605,7 @@ class _WorkflowInstanceImpl(  # type: ignore[reportImplicitAbstractClass]
                             defn.name,
                             defn.arg_types,
                             defn.dynamic_vararg,
+                            context,
                         )
                         handler_input.args = args
 
@@ -692,12 +698,17 @@ class _WorkflowInstanceImpl(  # type: ignore[reportImplicitAbstractClass]
                         )
 
                     # Create input
+                    context = temporalio.converter.WorkflowSerializationContext(
+                        namespace=self._info.namespace,
+                        workflow_id=self._info.workflow_id,
+                    )
                     args = self._process_handler_args(
                         job.query_type,
                         job.arguments,
                         defn.name,
                         defn.arg_types,
                         defn.dynamic_vararg,
+                        context,
                     )
                     input = HandleQueryInput(
                         id=job.query_id,
@@ -2102,6 +2113,7 @@ class _WorkflowInstanceImpl(  # type: ignore[reportImplicitAbstractClass]
         defn_name: Optional[str],
         defn_arg_types: Optional[List[Type]],
         defn_dynamic_vararg: bool,
+        context: Optional[temporalio.converter.SerializationContext] = None,
     ) -> List[Any]:
         # If dynamic old-style vararg, args become name + varargs of given arg
         # types. If dynamic new-style raw value sequence, args become name +
@@ -2109,15 +2121,15 @@ class _WorkflowInstanceImpl(  # type: ignore[reportImplicitAbstractClass]
         if not defn_name and defn_dynamic_vararg:
             # Take off the string type hint for conversion
             arg_types = defn_arg_types[1:] if defn_arg_types else None
-            return [job_name] + self._convert_payloads(job_input, arg_types)
+            return [job_name] + self._convert_payloads(job_input, arg_types, context)
         if not defn_name:
             return [
                 job_name,
                 self._convert_payloads(
-                    job_input, [temporalio.common.RawValue] * len(job_input)
+                    job_input, [temporalio.common.RawValue] * len(job_input), context
                 ),
             ]
-        return self._convert_payloads(job_input, defn_arg_types)
+        return self._convert_payloads(job_input, defn_arg_types, context)
 
     def _process_signal_job(
         self,
@@ -2125,12 +2137,17 @@ class _WorkflowInstanceImpl(  # type: ignore[reportImplicitAbstractClass]
         job: temporalio.bridge.proto.workflow_activation.SignalWorkflow,
     ) -> None:
         try:
+            context = temporalio.converter.WorkflowSerializationContext(
+                namespace=self._info.namespace,
+                workflow_id=self._info.workflow_id,
+            )
             args = self._process_handler_args(
                 job.signal_name,
                 job.input,
                 defn.name,
                 defn.arg_types,
                 defn.dynamic_vararg,
+                context,
             )
         except Exception:
             logger.exception(
