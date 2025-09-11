6de29b33f7eda5fa7406d52b7627ee39d118cc6c
Dan Davison <dan.davison@temporal.io>
2025-09-10 22:54:43 -0400

    test: Add test for activity context injection

--- tests/test_serialization_context.py
+++ tests/test_serialization_context.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 import dataclasses
 import uuid
 from dataclasses import dataclass, field
+from datetime import timedelta
 from typing import Any, Literal, Optional, Type
 
-from temporalio import workflow
+from temporalio import activity, workflow
 from temporalio.api.common.v1 import Payload
 from temporalio.client import Client
 from temporalio.converter import (
@@ -34,11 +35,27 @@ class TraceData:
     items: list[TraceItem] = field(default_factory=list)
 
 
+@activity.defn
+async def passthrough_activity(input: TraceData) -> TraceData:
+    return input
+
+
 @workflow.defn(sandboxed=False)  # we want to use isinstance
 class PassThroughWorkflow:
     @workflow.run
     async def run(self, input: TraceData) -> TraceData:
         return input
+
+
+@workflow.defn(sandboxed=False)
+class WorkflowWithActivity:
+    @workflow.run
+    async def run(self, input: TraceData) -> TraceData:
+        return await workflow.execute_activity(
+            passthrough_activity,
+            input,
+            start_to_close_timeout=timedelta(seconds=10),
+        )
 
 
 class SerializationContextTestEncodingPayloadConverter(
@@ -62,16 +79,23 @@ class SerializationContextTestEncodingPayloadConverter(
         return converter
 
     def to_payload(self, value: Any) -> Optional[Payload]:
+        if not isinstance(value, TraceData):
+            return None
         print(
             f"🌈 SerializationContextTestEncodingPayloadConverter.to_payload({value})"
         )
-        assert isinstance(value, TraceData)
-        assert isinstance(self.context, WorkflowSerializationContext)
-        value.items.append(
-            TraceItem(
-                context_type="workflow", method="to_payload", context=self.context
+        if isinstance(self.context, WorkflowSerializationContext):
+            value.items.append(
+                TraceItem(
+                    context_type="workflow", method="to_payload", context=self.context
+                )
             )
-        )
+        elif isinstance(self.context, ActivitySerializationContext):
+            value.items.append(
+                TraceItem(
+                    context_type="activity", method="to_payload", context=self.context
+                )
+            )
         payload = JSONPlainPayloadConverter().to_payload(value)
         assert payload
         payload.metadata["encoding"] = self.encoding.encode()
@@ -83,12 +107,18 @@ class SerializationContextTestEncodingPayloadConverter(
         )
         value = JSONPlainPayloadConverter().from_payload(payload, type_hint)
         assert isinstance(value, TraceData)
-        assert isinstance(self.context, WorkflowSerializationContext)
-        value.items.append(
-            TraceItem(
-                context_type="workflow", method="from_payload", context=self.context
+        if isinstance(self.context, WorkflowSerializationContext):
+            value.items.append(
+                TraceItem(
+                    context_type="workflow", method="from_payload", context=self.context
+                )
             )
-        )
+        elif isinstance(self.context, ActivitySerializationContext):
+            value.items.append(
+                TraceItem(
+                    context_type="activity", method="from_payload", context=self.context
+                )
+            )
         return value
 
 
@@ -137,3 +167,27 @@ async def test_workflow_payload_conversion_can_be_given_access_to_serialization_
         assert result.items[1].method == "from_payload"
         assert result.items[2].method == "to_payload"
         assert result.items[3].method == "from_payload"
+
+
+async def test_activity_payload_conversion_has_context(client: Client):
+    workflow_id = str(uuid.uuid4())
+    task_queue = str(uuid.uuid4())
+
+    config = client.config()
+    config["data_converter"] = data_converter
+    client = Client(**config)
+
+    async with Worker(
+        client,
+        task_queue=task_queue,
+        workflows=[WorkflowWithActivity],
+        activities=[passthrough_activity],
+    ):
+        result = await client.execute_workflow(
+            WorkflowWithActivity.run,
+            TraceData(),
+            id=workflow_id,
+            task_queue=task_queue,
+        )
+
+        assert any(item.context_type == "activity" for item in result.items)
